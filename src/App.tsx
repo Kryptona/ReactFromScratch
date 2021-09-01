1fce1cd9ca7fd79e9adf0b13d9f85106f595ce73
Дарья <dasha.seredenko.1997@gmail.com>
2021-09-01 15:59:36 +0400

    about

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './styles/defaults.scss'
 import Header from "./components/header/Header";
 import Panel from "./components/panel/Panel";
+import About from "./components/about/About";
 
 const App = () => (
     <div>
         <Header/>
         <Panel/>
+        <About/>
     </div>
 );
 
---
+++ src/components/about/About.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import styles from './about.scss';
+import deliver from '../../img/deliver.png';
+import manage from '../../img/manage.png';
+import plan from '../../img/plan.png';
+
+const About = () => {
+    return (
+        <div className={styles.root}>
+            <div className={styles.header}>
+                <div className={styles.section_name}>OUR STORY</div>
+                <div className={styles.band}/>
+            </div>
+            <div className={styles.information}>
+                <div className={styles.text}>
+                    <div className={styles.header}>Founded in 2011 by John Mathew Smith, <span>Konstruct</span> has
+                        become the number one
+                        construction
+                        management firm
+                    </div>
+                    <div className={styles.body}>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+                        nonummy nibh euismod
+                        tincidunt ut laoreet dolore magna aliquam erat. Lorem ipsum dolor sit amet, consectetuer
+                        adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.
+                    </div>
+                </div>
+                <div className={styles.images}>
+                    <div className={styles.image}>
+                        <img src={deliver} alt="deliver"/>
+                        <div className={styles.name}>
+                            WE <br/>PLAN
+                        </div>
+                    </div>
+                    <div className={styles.image}>
+                        <img src={manage} alt="manage"/>
+                        <div className={styles.name}>
+                            WE <br/>MANAGE
+                        </div>
+                    </div>
+                    <div className={styles.image}>
+                        <img src={plan} alt="plan"/>
+                        <div className={styles.name}>
+                            WE <br/>BUILD
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default About;
